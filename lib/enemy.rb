88216dcdc0b582c3e3544af042aa8382fcf16625
Won Kim <wonsae.kim@gmail.com>
2019-05-07 10:51:40 -0400

    Fix user class and add stats to Enemy class

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -1,4 +1,13 @@
 class Enemy < ActiveRecord::Base
   belongs_to :stage
   has_many :users, through: :stages
+
+  def save_stats(hero_hash)
+    self.name = hero_hash['name']
+    self.hp = hero_hash['powerstats']['durability']
+    self.atk = hero_hash['powerstats']['strength']
+    self.def = hero_hash['powerstats']['durability']
+    self.speed = hero_hash['powerstats']['speed']
+    self.save
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
   belongs_to :stage
   has_many :enemies, through: :stages
 
-  def user_stats(superhero_hash)
-    user.superhero_name = hero_hash['name']
-    user.hp = hero_hash['powerstats']['durability']
-    user.atk = hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']
-    user.def = hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']
-    user.speed = hero_hash['powerstats']['speed']
-    user.save
+  def user_stats(hero_hash)
+    self.superhero_name = hero_hash['name']
+    self.hp = hero_hash['powerstats']['durability']
+    self.atk = hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']
+    self.def = hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']
+    self.speed = hero_hash['powerstats']['speed']
+    self.save
   end
 
 end
