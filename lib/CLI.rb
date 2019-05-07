d3a232f3f6393941345d91d7a315b66bd41e85a1
Won Kim <wonsae.kim@gmail.com>
2019-05-07 10:46:55 -0400

    Add victory/gameover

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -3,39 +3,14 @@ def welcome
   puts "A bearded wizard appears"
   puts "Bearded Wizard: What the heck? Who are you? Why are you in my special place?"
   puts "Enter your name:"
-  name = gets.chomp #needs to be saved
-  #find or create user id
+  
+  name = gets.chomp
   user = User.find_or_create_by(name: name)
 
   puts "Bearded Wizard: So... for some reason you have entered the Superhero Fighting Arena"
   puts "Bearded Wizard: CHOOSE YOUR SUPERHERO AND FIGHT TO THE DEATH or to the end of the stages."
 
   choose_a_hero(user)
-end
-
-def confirm(hero_hash, user)
-  puts "#{hero_hash['name']} stats:\n"
-  puts "HP: #{hero_hash['powerstats']['durability']}\n"
-  puts "ATK: #{hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']}\n"
-  puts "DEF: #{hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']}\n"
-  puts "SPEED: #{hero_hash['powerstats']['speed']}\n"
-  puts "Bearded Wizard: Oh interesting... are you sure you want to pick this superhero?"
-  puts "Type (Y) to confirm or (N) to go back and choose another superhero."
-
-  input = gets.chomp.downcase
-  loop do
-    if input == 'y'
-      #call stage
-      stage(user)
-      break
-    elsif input == 'n'
-      #go back to choose_a_hero
-      choose_a_hero(user)
-      break
-    else
-      puts "Beard Wizard: QUIT MESSING AROUND! TYPE IN Y OR N!"
-    end
-  end
 end
 
 def choose_a_hero(user)
@@ -57,20 +32,71 @@ def choose_a_hero(user)
   end
 end
 
+def confirm(hero_hash, user)
+  puts "#{hero_hash['name']} stats:\n"
+  puts "HP: #{hero_hash['powerstats']['durability']}\n"
+  puts "ATK: #{hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']}\n"
+  puts "DEF: #{hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']}\n"
+  puts "SPEED: #{hero_hash['powerstats']['speed']}\n"
+  puts "Bearded Wizard: Oh interesting... are you sure you want to pick this superhero?"
+  puts "Type (Y) to confirm or (N) to go back and choose another superhero."
+
+  input = gets.chomp.downcase
+  loop do
+    if input == 'y'
+      puts "Bearded Wizard: I guess that's a good choice..."
+      #save user's superhero choice and stats
+      user.superhero_name = hero_hash['name']
+      user.hp = hero_hash['powerstats']['durability']
+      user.atk = hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']
+      user.def = hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']
+      user.speed = hero_hash['powerstats']['speed']
+      user.save
+      #call stage
+      stage(user)
+      break
+    elsif input == 'n'
+      #go back to choose_a_hero
+      choose_a_hero(user)
+      break
+    else
+      print "Beard Wizard: QUIT MESSING AROUND! TYPE IN Y OR N!"
+    end
+  end
+end
+
 def stage(user)
-  stats_string = RestClient.get('https://akabab.github.io/superhero-api/api/all.json')
-  stats_array = JSON.parse(stats_string)
-  sample = stats_array.sample(1)
-  name = sample[0]["name"]
-  int = sample[0]["powerstats"]["intelligence"]
-  str = sample[0]["powerstats"]["strength"]
-  spd = sample[0]["powerstats"]["speed"]
-  dur = sample[0]["powerstats"]["durability"]
-  pwr = sample[0]["powerstats"]["power"]
-  cbt = sample[0]["powerstats"]["combat"]
+  print "Bearded Wizard: Well then, it's time to FIGHT TO THE DEATH!"
+  print "STAGE #{stage_number} BEGIN!"
 
-  Enemy.find_or_create_by(name: name, hp: dur, atk: pwr, def: cbt, speed: spd )
+  new_stage = Stage.new(user: user)
+  won = new_stage.battle
 
-  #give them stat
-  #loop till someone's hp reach 0
+  if won
+    victory(user)
+  else
+    game_over(user)
+  end
+end
+
+def victory(user)
+  print "Bearded Wizard: Woah! You actually won! That's incredible... congrats"
+  print "VICTORY!"
+
+end
+
+def game_over(user)
+  print "Bearded Wizard: Heh, figured you lose, nice try"
+  print "Want to try again?"
+  print "Type (Y) to start over and try again or (N) to stop playing"
+
+  input = gets.chomp.downcase
+  loop do
+    if input == 'y'
+      choose_a_hero(user)
+      break
+    elsif input == 'n'
+      break
+    end
+  end
 end
