a8096935d2ff7bc8ab5a4f0c17e5925d7c7d6e72
Won Kim <wonsae.kim@gmail.com>
2019-05-08 13:54:32 -0400

    refactor cli, enemy, stage, user

--- lib/user.rb
+++ lib/user.rb
@@ -3,15 +3,14 @@ class User < ActiveRecord::Base
   has_many :enemies, through: :stages
 
   def save_stats(hero_hash)
-    self.superhero_name = hero_hash['name']
-    self.hp = hero_hash['powerstats']['durability']
-    self.atk = hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat']
-    self.def = hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence']
-    self.speed = hero_hash['powerstats']['speed']
-    self.temp_def = 0
-    self.max_hp = hero_hash['powerstats']['durability']
-    self.stage_level = 1
-    self.save
+    self.update(superhero_name: hero_hash['name'],
+      hp: hero_hash['powerstats']['durability'],
+      atk: hero_hash['powerstats']['strength'] + hero_hash['powerstats']['combat'],
+      def: hero_hash['powerstats']['durability'] + hero_hash['powerstats']['intelligence'],
+      speed: hero_hash['powerstats']['speed'],
+      temp_def: 0,
+      max_hp: hero_hash['powerstats']['durability'],
+      stage_level: 1)
   end
 
 end
